c5bbcadd3592a7c50e599bd56da33a01cedcbb95
haton14 <haton14@haton14.com>
2023-10-12 00:09:59 +0900

    工事中

---
+++ .zshenv
@@ -0,0 +1,3 @@
+# Go
+export PATH=/usr/local/go/bin:$PATH
+export PATH=$HOME/go/bin:$PATH
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+git config --global core.pager cat
+wget https://go.dev/dl/go1.21.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
+ln -s .zshenv ~/.zshenv
