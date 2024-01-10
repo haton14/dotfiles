c75e642dbf92e90c43bd4576fbdf304e06b8e8b3
haton14 <haton14@haton14.com>
2024-01-10 12:24:26 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 git config --global core.pager cat
 
 # Go
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
-sudo rm go1.21.5.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+sudo rm go1.21.6.linux-amd64.tar.gz
 
 # Git
 git config --global user.name "haton14"
